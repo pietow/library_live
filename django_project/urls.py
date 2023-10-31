79d504eacd29a599b9832711a4eae8fd1920b578
piet <pprotassow@posteo.de>
2023-10-31 13:03:24 +0100

    lib api

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('apis.urls')), # new
     path('', include('books.urls')), # new
 ]
